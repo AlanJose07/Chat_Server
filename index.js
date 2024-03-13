2f71bb9d63367d051806a7f6f2f1723643f789b0
Alan Jose <123865891+AlanJose07@users.noreply.github.com>
2024-03-14 00:26:58 +0530

    Update index.js - Orgin https://chat-server-3gs8.onrender.com allowing

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const userRooms = new Map();
 
 const allowedOriginList = ['http://localhost:3000',
     'http://localhost:3000', 'http://127.0.0.1:5501/testfile.html',
-    'http://127.0.0.1:5501', 'http://127.0.0.1:5501', 'http://127.0.0.1:5500', 'http://127.0.0.1:5500'
+    'http://127.0.0.1:5501', 'http://127.0.0.1:5501', 'http://127.0.0.1:5500', 'http://127.0.0.1:5500','https://chat-server-3gs8.onrender.com'
 ];
 
 if (process.env.DB_CONNECTION != "OFF") {
@@ -60,7 +60,7 @@ function dbCall(res, uri) {
 }
 const corsOptions = {
     origin: ['http://example.com', 'http://another-example.com', 'http://127.0.0.1:5501',
-        'http://127.0.0.1:5500', 'http://127.0.0.1:5500/'], // Array of allowed origins
+        'http://127.0.0.1:5500', 'http://127.0.0.1:5500/','https://chat-server-3gs8.onrender.com'], // Array of allowed origins
 };
 
 
